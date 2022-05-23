22f9277fc23225cf329b10ca2f2f75df94b32142
gk <gopikrishna169@gmail.com>
2022-05-24 03:01:24 +0530

    Update landingPage.jsx

--- src/pages/landingPage/landingPage.jsx
+++ src/pages/landingPage/landingPage.jsx
@@ -3,6 +3,7 @@ import './landingPage.css'
 import button from "../../images/B1.png";
 import logo from "../../images/logo.png";
 import { Link } from 'react-router-dom';
+import video from "../../images/video.mp4";
 class LandingPage extends Component {
 
     constructor(props) {
@@ -17,7 +18,9 @@ class LandingPage extends Component {
         return (
             <React.Fragment>
                 <div className="landing-page">
-                    <iframe className="video-background" src="https://www.youtube.com/embed/VFB5r6kpxXk?version=3&loop=1&autoplay=1&controls=0&showinfo=0&vq=hd1080" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <video className="video" autoPlay loop>
+                        <source src={video}/>
+                    </video>
                     <div className="headings">
                         <img className="logo" src={logo} alt=""/>
                         <h1 className="heading-3">Check your cyber security skills to the test and help Company X.</h1>
