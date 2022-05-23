ba1d7fdb858eac31002dff998f933af6df0fecce
gk <gopikrishna169@gmail.com>
2022-05-24 03:03:11 +0530

    Update introPage.jsx

--- src/pages/introPage/introPage.jsx
+++ src/pages/introPage/introPage.jsx
@@ -4,12 +4,16 @@ import button from "../../images/B2.png";
 import user from "../../images/User1.png";
 import { Link } from 'react-router-dom';
 import logo from "../../images/logo.png";
+import video from "../../images/video.mp4";
+
 class IntroPage extends Component {
     render() {
         return (
             <React.Fragment>
                 <div className="intro-page">
-                    <iframe className="video-background" src="https://www.youtube.com/embed/VFB5r6kpxXk?version=3&loop=1&autoplay=1&controls=0&showinfo=0&vq=hd1080" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <video className="video" autoPlay loop>
+                        <source src={video} />
+                    </video>
                     <div className="headings">
                     <img className="logo" src={logo} alt=""/>
                         <h1 className="heading-3">Check your cyber security skills to the test and help Company X.</h1>
